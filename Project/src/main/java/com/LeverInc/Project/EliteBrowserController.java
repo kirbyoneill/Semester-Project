a6ea671ea2e6749554d666193551c969c942a5b7
kirbyoneill <dkoneill1594@comcast.net>
2015-12-01 20:59:22 -0500

    functional address bar; button useless

--- Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
+++ Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
@@ -1,21 +1,42 @@
 package com.LeverInc.Project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
+import javafx.scene.control.*;
+import javafx.scene.web.*;
+import javafx.scene.input.*;
 import javafx.scene.layout.*;
 
 public class EliteBrowserController extends Region{
-
+	public String address = "http://www.google.com";
+	
     @FXML
     private WebView webView;
 
     @FXML
     private TextField tfAddressBar;
-    
+
+    @FXML
+    private Button btnGo;
+
     public void initialize(){
     WebEngine webEngine = webView.getEngine();
-    webEngine.load("http://www.google.com");
+    tfAddressBar.setText(address);
+    webEngine.load(address);
     }
+    
+    @FXML
+    void onGoEntered(KeyEvent event) {
+    	address = tfAddressBar.getText();
+    	if(event.getCode() == KeyCode.ENTER) {
+    		WebEngine webEngine = webView.getEngine();
+    	    webEngine.load(address);
+    	}
+    }
+
+    @FXML
+    void onGoClicked(ActionEvent event) {
+
+    }
+
 }
