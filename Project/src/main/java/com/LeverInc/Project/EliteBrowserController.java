04dbe0b92f60e1ae48d2f2533d9653fe0b58bab2
SK207_33 <SK207_33@K207_W833.carrollcc.edu>
2015-12-03 20:57:35 -0500

    added menu items

--- Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
+++ Project/src/main/java/com/LeverInc/Project/EliteBrowserController.java
@@ -1,11 +1,17 @@
 package com.LeverInc.Project;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.web.*;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
 public class EliteBrowserController extends Region{
 	public String address = "http://www.google.com";
@@ -25,6 +31,12 @@ public class EliteBrowserController extends Region{
     @FXML
     private MenuItem close;
 
+    @FXML
+    private MenuItem about;
+
+    @FXML
+    private MenuItem newWindow;
+
     public void initialize(){
     WebEngine webEngine = webView.getEngine();
     tfAddressBar.setText(address);
@@ -40,7 +52,23 @@ public class EliteBrowserController extends Region{
     	}
     }
 
+    @FXML
+    void aboutButton(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("about.fxml"));
+    	Stage stage = new Stage();
+    	stage.setScene(new Scene(root, 200, 400));
+    	stage.show();
+    }
 
+    @FXML
+    void openInstance(ActionEvent event) throws IOException {
+    	Stage stage = new Stage();
+    	Parent parent = FXMLLoader.load(getClass().getResource("EliteBrowser.fxml"));
+		Scene scene = new Scene(parent);
+		stage.setTitle("Elite Browser");
+		stage.setScene(scene);
+		stage.show();
+    }
     @FXML
     void browserClose(ActionEvent event) {
     	System.exit(0);
